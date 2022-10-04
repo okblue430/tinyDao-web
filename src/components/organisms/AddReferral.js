60b12aed6f5160a8a8cf966c444f2ca78381796e
okBlue <bluedreampro2017@gmail.com>
2022-10-04 09:44:15 +0800

    update type of referral balance

--- src/components/organisms/AddReferral.js
+++ src/components/organisms/AddReferral.js
@@ -62,8 +62,8 @@ export function AddReferral ({
         const donation = new ethers.Contract(addressContract, DonationAbi, signer)
         try {
             const balance = await donation.getReferralBalance(currentAccount) 
-            console.log(showBalance(balance))
-            setReferralBalance(showBalance(balance))
+            console.log(balance)
+            setReferralBalance(balance)
             // console.log({res})
         } catch (error) {
             console.log("deposite error", error)            
